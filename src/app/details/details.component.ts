8369926982e8af1699dab4a58b416bad61be8ed9
Klimek <klimkowskib@gmail.com>
2020-04-20 21:22:17 +0200

    fix app and mainComp add id routing in detailsComp

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FilmyService} from '../filmy.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  
+  
 
-  constructor() { }
+  constructor( private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log('id',id);
+    
+
   }
 
 }
