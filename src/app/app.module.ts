10c4ed6fe178a9f90ad363339242b2f83cb3f46d
Klimek <klimkowskib@gmail.com>
2020-04-20 19:11:41 +0200

    first, create details comp, main comp and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
