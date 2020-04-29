c7798ea34c81f26bab40db4d9ed12fdbf0fe13a1
Klimek <klimkowskib@gmail.com>
2020-04-29 18:43:14 +0200

    Authorization Token with httpHeaders

--- src/app/filmy.service.ts
+++ src/app/filmy.service.ts
@@ -12,14 +12,21 @@ export class FilmyService {
 
   private baseUrl: String = environment.apiURL
 
+  private token: String = '8c7ebea09b0a345dc3fae92c042a8e0b219d25ef'
+  private httpHeaders = new HttpHeaders(
+    {'Authorization':`Token ${this.token}`}
+    )
+
   constructor(private http: HttpClient) { }
 
   wszystkieFilmy(): Observable<any> {
-    return this.http.get(this.baseUrl + '/api/filmy/') ;
+    return this.http.get(this.baseUrl + '/api/filmy/', 
+    {headers: this.httpHeaders}) ;
   }
 
   getFilm(id) : Observable<any>{
-    return this.http.get(`${this.baseUrl}/api/filmy/${id}/` );
+    return this.http.get(`${this.baseUrl}/api/filmy/${id}/`,
+    {headers: this.httpHeaders} );
 
   }
 }
